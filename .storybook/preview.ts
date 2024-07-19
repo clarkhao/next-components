84e06a3603eb3947eadf5026694cfda31dd2f3f9
clarkhao <clarktotoro@gmail.com>
2024-07-19 17:08:40 +0800

    build: update the storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,17 +1,21 @@
+import "../src/app/globals.css"
 import type { Preview } from "@storybook/react"
-
-import "../styles/tailwind.css"
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
+        date: /Date$/i,
       },
     },
+    nextjs: {
+      appDirectory: true,
+    },
   },
+
+  decorators: [],
+  loaders: [],
 }
 
 export default preview
